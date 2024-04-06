fe42a016f4299d23f7c2109964c5c561041fee1b
Kim@PC <kimdeguzman20@yahoo.com>
2024-04-06 15:43:08 +0800

    Exception Handling
    - Remove error reporting
    - Custom exception renderer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +15,23 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReport([]);
+
+        $exceptions->stopIgnoring([HttpException::class]);
+
+        $exceptions->report(function (Exception $e) {})->stop();
+
+        $exceptions->render(function(Exception $exception, \Illuminate\Http\Request $request){
+            \Illuminate\Support\Facades\Log::info([
+                'Render Request' => $request->url(),
+                'Render Exception Class' => get_class($exception),
+                'Render Exception Message' => $exception->getMessage()
+            ]);
+
+            return response()->json([
+                'message' => $exception->getMessage()
+            ]);
+        });
+
+
     })->create();
