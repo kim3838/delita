fdfab732519bec849ba271bedfbb8c6eba8b3e4b
Kim@PC <kimdeguzman20@yahoo.com>
2024-04-13 10:34:51 +0800

    Install Sanctum Api
    - Remove frontend resources
    - Ensure email is verified middleware
    - Publish cors config
    - App bootstrap: api route, stateful api middleware and verified email middleware
    - User model use Api Tokens trait

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,11 +26,16 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi();
+
+        $middleware->alias([
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->dontReport([]);
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function(){
+
+    Route::get('/user', function (Request $request) {
+        return \App\Facades\ResponseJson::successfulResponse($request->user());
+    });
+});
+
