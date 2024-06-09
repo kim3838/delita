faacbfacc4713ab87995dac65a9a92fd80342b7e
Kim@PC <kimdeguzman20@yahoo.com>
2024-06-09 11:02:22 +0800

    Publish Language Files

---
+++ lang/en/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'email.verified' => 'Email verified',
+    'email.already.verified' => 'Email already verified',
+    'email.verification.sent' => 'Verification link sent to :email',
+    'email.verification.error' => 'Verification confirmation/check neither verified nor marked as verified',
+];
