53f3f085b403bc09dae4b0eb9c432497892806e5
Kim@PC <kimdeguzman20@yahoo.com>
2025-04-08 11:07:25 +0800

    Database Seeder Changes
    - Add super admin user type
    - Add 9 more users
    - Add 500 prototypes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserType;
+use App\Models\Prototype;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'kim.123',
+            'email' => 'luxere20@gmail.com',
+            'type' => UserType::SUPER_ADMIN
         ]);
+
+        User::factory(9)->create();
+
+        Prototype::factory()->count(500)->create();
     }
 }
