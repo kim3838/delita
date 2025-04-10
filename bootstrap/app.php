d0f3b80ef3fd98ab3ad1cef9e9467cb694f6e445
Kim@PC <kimdeguzman20@yahoo.com>
2025-04-10 14:10:22 +0800

    Change Access Denied Exception Response From Unauthorized 401 into Forbidden 403

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -85,7 +85,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     $throwable instanceof MethodNotAllowedHttpException => ResponseJson::methodNotAllowedResponse(),
                     $throwable instanceof InvalidArgumentException => ResponseJson::validationErrorResponse([], $throwable->getMessage()),
                     $throwable instanceof InvalidSignatureException => ResponseJson::unprocessableResponse([], $throwable->getMessage()),
-                    $throwable instanceof AccessDeniedHttpException => ResponseJson::unauthorizedResponse($throwable->getMessage()),
+                    $throwable instanceof AccessDeniedHttpException => ResponseJson::forbiddenResponse($throwable->getMessage()),
                     default => $throwable,
                 };
 
