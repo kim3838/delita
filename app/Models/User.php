8dfdd4dc39aa6c051b239496c8a95a81502faf68
Kim@PC <kimdeguzman20@yahoo.com>
2025-04-10 16:20:42 +0800

    User Additions
    - Add hidden attributes
    - Update casts attributes
    - New attribute accessors

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\UserType;
 use App\Traits\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -37,7 +39,13 @@ class User extends Authenticatable implements MustVerifyEmail
         'two_factor_recovery_codes',
         'remember_token',
         'created_at',
-        'updated_at'
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $appends = [
+        'two_factor_enabled',
+        'two_factor_confirmed'
     ];
 
     /**
@@ -48,8 +56,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'email_verified_at' => 'datetime:Y-m-d H:i:s',
             'password' => 'hashed',
+            'type' => UserType::class,
         ];
     }
 
@@ -58,5 +67,29 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->belongsToMany(Company::class)
             ->withPivot('assignment_type')
             ->withTimestamps();
+    }
+
+    public function twoFactorEnabled(): Attribute
+    {
+        return new Attribute(
+            get: fn() => !is_null($this->two_factor_secret),
+        );
+    }
+
+    public function twoFactorConfirmed(): Attribute
+    {
+        return new Attribute(
+            get: fn() => !is_null($this->two_factor_confirmed_at),
+        );
+    }
+
+    public function isSuperAdmin(): bool
+    {
+        return $this->type == UserType::SUPER_ADMIN;
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->type == UserType::ADMIN;
     }
 }
