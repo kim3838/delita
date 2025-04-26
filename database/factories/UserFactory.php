f86079a76d1c8368fa312ab0eb1f3ea4f855b770
Kim@PC <kimdeguzman20@yahoo.com>
2025-04-26 11:58:40 +0800

    Update User Factory Default Password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('hr56FHDGF33rdf23$%3'),
             'type' => UserType::DEFAULT,
             'remember_token' => Str::random(10),
         ];
