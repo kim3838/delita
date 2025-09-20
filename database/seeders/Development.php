ccb6145db34261327edb003258cd553ed86bb0c0
Kim@PC <kimdeguzman20@yahoo.com>
2025-09-20 17:30:50 +0800

    Remove Is Lunch Break Compensable From Development Seeder
    - Drop `is_lunch_break_compensable` field from `DevelopmentSeeder` logic.
    - Adjust seeder entries to align with removed field.

--- database/seeders/Development.php
+++ database/seeders/Development.php
@@ -459,8 +459,7 @@ class Development extends Seeder
                     'has_lunch_break' => false,
                     'lunch_break_start' => null,
                     'lunch_break_end' => null,
-                    'total_lunch_break_hours' => null,
-                    'is_lunch_break_compensable' => false
+                    'total_lunch_break_hours' => null
                 ]);
 
             } else {
@@ -477,8 +476,7 @@ class Development extends Seeder
                     'has_lunch_break' => true,
                     'lunch_break_start' => '12:00',
                     'lunch_break_end' => '13:00',
-                    'total_lunch_break_hours' => '01:00',
-                    'is_lunch_break_compensable' => false
+                    'total_lunch_break_hours' => '01:00'
                 ]);
             }
         }
