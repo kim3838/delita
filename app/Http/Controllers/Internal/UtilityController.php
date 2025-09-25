aaa3e3040b8081dc5f9e0c170237c2a91cb42e83
Kim@PC <kimdeguzman20@yahoo.com>
2025-09-25 10:32:51 +0800

    Add Debug and Utility Methods to UtilityController
    - Add `debugCalendarAssets` method for debugging calendar-related data.
    - Add `debugAuthorization` method to test authorization mechanisms.
    - Add `debugTimezones` method to analyze timezone-related functionality and user timezones.
    - Add `interferePersistCompany` method to handle and manipulate `persist_company` cookie values.

--- app/Http/Controllers/Internal/UtilityController.php
+++ app/Http/Controllers/Internal/UtilityController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Internal;
 
 use App\Facades\ResponseJson;
 use App\Http\Controllers\Controller;
+use App\Models\Prototype;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class UtilityController extends Controller
 {
@@ -20,6 +23,61 @@ class UtilityController extends Controller
 
     public function debug(Request $request)
     {
+        return $this->debugTimezones($request);
+    }
+
+    public function debugCalendarAssets()
+    {
+        $now = Carbon::now();
+
+        _debug([
+            '$now' => $now->toDateTimeString(),
+            '$now day of week' => $now->dayOfWeek,
+            'monday' => Carbon::THURSDAY,
+            'weekday' => $now->weekday(),
+        ]);
+
         return ResponseJson::successfulResponse();
+    }
+
+    public function debugAuthorization()
+    {
+        Gate::authorize('create', Prototype::class);
+
+        return ResponseJson::successfulResponse();
+    }
+
+    public function debugTimezones(Request $request)
+    {
+        $globalCarbon = Carbon::parse('now');
+
+        $dateTimeAsIs = Carbon::createFromFormat('Y-m-d H:i:s', '2025-09-02 10:00:00', 'Asia/Manila');
+
+        $response = [
+            '01 Auth Timezone' => $request->user()?->timezone ?? 'Unauthenticated',
+            '02 Default Timezone' => date_default_timezone_get(),
+            '03 Global Carbon Now' => $globalCarbon->toArray(),
+            ...($request->user()?->timezone ? [
+                '04 Global to Auth Timezone' => $globalCarbon->setTimezone($request->user()?->timezone)->toDateTimeString(),
+            ] : []),
+        ];
+
+        return ResponseJson::successfulResponse($response);
+    }
+
+    public function interferePersistCompany(Request $request)
+    {
+        //Set `persist_company` @ bootstrap/app.php $middleware->encryptCookies
+        $persistCompany = $request->cookie('persist_company');
+
+        $persistCompany = (int)$persistCompany;
+        $persistCompany++;
+
+        //Cookie::queue(Cookie::make('persist_company', $persistCompany, 60, '/', '.server.local', 'false', 'false'));
+        header('Set-Cookie: persist_company='.$persistCompany.'; Path=/; Domain=.server.local; Max-Age=3600; SameSite=Lax');
+
+        return ResponseJson::successfulResponse([
+            'persist_company' => $persistCompany
+        ]);
     }
 }
