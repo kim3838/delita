acc3b25bbef9a8125eff8aa2ace30dc7c833e7b1
Kim@PC <kimdeguzman20@yahoo.com>
2025-09-30 08:59:03 +0800

    Add Overtime Rate Types in HourlyRateType Enum
    - Extend `HourlyRateType` enum with new overtime classifications, including regular, holidays, and night rates.
    - Update `label` method to include descriptions for added cases.

--- app/Enums/HourlyRateType.php
+++ app/Enums/HourlyRateType.php
@@ -22,6 +22,24 @@ enum HourlyRateType: int implements BaseEnum
     case NIGHT_DOUBLE_HOLIDAY = 206;
     case NIGHT_REST_DOUBLE_HOLIDAY = 207;
 
+    case OVERTIME_REGULAR = 300;
+    case OVERTIME_REST = 301;
+    case OVERTIME_SPECIAL_HOLIDAY = 302;
+    case OVERTIME_REST_SPECIAL_HOLIDAY = 303;
+    case OVERTIME_LEGAL_HOLIDAY = 304;
+    case OVERTIME_REST_LEGAL_HOLIDAY = 305;
+    case OVERTIME_DOUBLE_HOLIDAY = 306;
+    case OVERTIME_REST_DOUBLE_HOLIDAY = 307;
+
+    case OVERTIME_NIGHT_REGULAR = 400;
+    case OVERTIME_NIGHT_REST = 401;
+    case OVERTIME_NIGHT_SPECIAL_HOLIDAY = 402;
+    case OVERTIME_NIGHT_REST_SPECIAL_HOLIDAY = 403;
+    case OVERTIME_NIGHT_LEGAL_HOLIDAY = 404;
+    case OVERTIME_NIGHT_REST_LEGAL_HOLIDAY = 405;
+    case OVERTIME_NIGHT_DOUBLE_HOLIDAY = 406;
+    case OVERTIME_NIGHT_REST_DOUBLE_HOLIDAY = 407;
+
     public function label(): string
     {
         return match ($this) {
@@ -33,6 +51,7 @@ enum HourlyRateType: int implements BaseEnum
             self::REST_LEGAL_HOLIDAY => 'Legal Holiday & Rest Day',
             self::DOUBLE_HOLIDAY => 'Double holiday',
             self::REST_DOUBLE_HOLIDAY => 'Double Holiday & Rest Day',
+
             self::NIGHT_REGULAR => 'Night Regular',
             self::NIGHT_REST => 'Night Rest Day',
             self::NIGHT_SPECIAL_HOLIDAY => 'Night Special holiday',
@@ -41,6 +60,24 @@ enum HourlyRateType: int implements BaseEnum
             self::NIGHT_REST_LEGAL_HOLIDAY => 'Night Legal Holiday & Night Rest Day',
             self::NIGHT_DOUBLE_HOLIDAY => 'Night Double holiday',
             self::NIGHT_REST_DOUBLE_HOLIDAY => 'Night Double Holiday & Night Rest Day',
+
+            self::OVERTIME_REGULAR => 'Overtime Regular',
+            self::OVERTIME_REST => 'Overtime Rest Day',
+            self::OVERTIME_SPECIAL_HOLIDAY => 'Overtime Special holiday',
+            self::OVERTIME_REST_SPECIAL_HOLIDAY => 'Overtime Special Holiday & Overtime Rest Day',
+            self::OVERTIME_LEGAL_HOLIDAY => 'Overtime Legal holiday',
+            self::OVERTIME_REST_LEGAL_HOLIDAY => 'Overtime Legal Holiday & Overtime Rest Day',
+            self::OVERTIME_DOUBLE_HOLIDAY => 'Overtime Double holiday',
+            self::OVERTIME_REST_DOUBLE_HOLIDAY => 'Overtime Double Holiday & Overtime Rest Day',
+
+            self::OVERTIME_NIGHT_REGULAR => 'Overtime Night Regular',
+            self::OVERTIME_NIGHT_REST => 'Overtime Night Rest Day',
+            self::OVERTIME_NIGHT_SPECIAL_HOLIDAY => 'Overtime Night Special holiday',
+            self::OVERTIME_NIGHT_REST_SPECIAL_HOLIDAY => 'Overtime Night Special Holiday & Overtime Night Rest Day',
+            self::OVERTIME_NIGHT_LEGAL_HOLIDAY => 'Overtime Night Legal holiday',
+            self::OVERTIME_NIGHT_REST_LEGAL_HOLIDAY => 'Overtime Night Legal Holiday & Overtime Night Rest Day',
+            self::OVERTIME_NIGHT_DOUBLE_HOLIDAY => 'Overtime Night Double holiday',
+            self::OVERTIME_NIGHT_REST_DOUBLE_HOLIDAY => 'Overtime Night Double Holiday & Overtime Night Rest Day',
         };
     }
 
