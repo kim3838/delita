8617c0e00422b8496611ffd7ebfadcd41754a533
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-20 10:24:13 +0800

    Refactor WorkPeriod Trait for Improved Formula Handling
    - Add `resolveCompanyFormulaSettings` method to centralize formula resolution logic.
    - Introduce dedicated properties for various company formulas and rates.
    - Refactor methods to utilize the new `getCompanyFormulaSettings` method for streamlined logic.
    - Enhance null-checking and error prevention across methods.
    - Add `shiftOvertimeLimit` property initialization in relevant methods.

--- app/Traits/WorkPeriod.php
+++ app/Traits/WorkPeriod.php
@@ -16,28 +16,31 @@ use Illuminate\Support\Collection;
 
 trait WorkPeriod
 {
-    function companyBasicSalaryFormulaSettings(): Collection
+    protected ?Collection $companyBasicSalaryFormulaSettings = null;
+    protected ?Collection $companyOvertimeFormulaSettings = null;
+    protected ?Collection $basicSalaryRegularRates = null;
+    protected ?Collection $basicSalaryNightDifferentialRates = null;
+    protected ?Collection $overtimeRegularRates = null;
+    protected ?Collection $overtimeNightDifferentialRates = null;
+
+    function resolveCompanyFormulaSettings(): void
     {
-        $company = clone $this->company;
-
-        $companyBasicSalaryFormula = $company
-            ->formulas
-            ->where('formulable_type', Formulable::EARNINGS)
-            ->where('component_type' , Compensation::BASIC_SALARY)
-            ->first()
-            ->pivot;
-
-        $companyBasicSalaryFormulaHydrated = app(CompanyFormulaRepository::class)
-            ->hydrateItem($companyBasicSalaryFormula->toArray());
-
-        return collect($companyBasicSalaryFormulaHydrated->settings->parsed)
-            ->sortBy('order');
+        $this->companyBasicSalaryFormulaSettings = $this->companyBasicSalaryFormulaSettings();
+        $this->companyOvertimeFormulaSettings = $this->companyOvertimeFormulaSettings();
+        $this->basicSalaryRegularRates = $this->getBasicSalaryRegularRates();
+        $this->basicSalaryNightDifferentialRates = $this->getBasicSalaryNightDifferentialRates();
+        $this->overtimeRegularRates = $this->getOvertimeRegularRates();
+        $this->overtimeNightDifferentialRates = $this->getOvertimeNightDifferentialRates();
     }
 
     function resolveCompanyNightHoursFromBasicSalaryFormulaSettings(): void
     {
+        if(empty($this->companyBasicSalaryFormulaSettings)){
+            return;
+        }
+
         $companyNightDifferentialHours = collect(
-            $this->companyBasicSalaryFormulaSettings()
+            $this->companyBasicSalaryFormulaSettings
                 ->where('key', 'night_differential_hours')
                 ->first()
                 ->value
@@ -50,10 +53,44 @@ trait WorkPeriod
         $this->nightEnd = !empty($nightEnd) ? Carbon::parse($nightEnd)->format('H:i') : null;
     }
 
-    function getBasicSalaryRegularRates(): Collection
+    private function getCompanyFormulaSettings($formulableType, $componentType): ?Collection
     {
+        $company = clone $this->company;
+        $companyFormula = $company
+            ->formulas
+            ->where('formulable_type', $formulableType)
+            ->where('component_type', $componentType)
+            ->first();
+
+        if (empty($companyFormula)) {
+            return null;
+        }
+
+        $companyFormulaHydrated = app(CompanyFormulaRepository::class)
+            ->hydrateItem($companyFormula->pivot->toArray());
+
+        return collect($companyFormulaHydrated->settings->parsed)
+            ->sortBy('order');
+    }
+
+    function companyBasicSalaryFormulaSettings(): ?Collection
+    {
+        return $this->getCompanyFormulaSettings(Formulable::EARNINGS->value, Compensation::BASIC_SALARY->value);
+    }
+
+    function companyOvertimeFormulaSettings(): ?Collection
+    {
+        return $this->getCompanyFormulaSettings(Formulable::EARNINGS->value, Compensation::OVERTIME->value);
+    }
+
+    function getBasicSalaryRegularRates(): ?Collection
+    {
+        if(empty($this->companyBasicSalaryFormulaSettings)){
+            return null;
+        }
+
         $companyBasicSalaryRegularRates = collect(
-            $this->companyBasicSalaryFormulaSettings()
+            $this->companyBasicSalaryFormulaSettings
                 ->where('key', 'regular_rates')
                 ->first()
                 ->value
@@ -81,10 +118,14 @@ trait WorkPeriod
         });
     }
 
-    function getBasicSalaryNightDifferentialRates(): Collection
+    function getBasicSalaryNightDifferentialRates(): ?Collection
     {
+        if(empty($this->companyBasicSalaryFormulaSettings)){
+            return null;
+        }
+
         $companyBasicSalaryNightDifferentialRates = collect(
-            $this->companyBasicSalaryFormulaSettings()
+            $this->companyBasicSalaryFormulaSettings
                 ->where('key', 'night_differential_rates')
                 ->first()
                 ->value
@@ -101,6 +142,76 @@ trait WorkPeriod
                 'legal_holiday_and_rest_day' => HourlyRateType::NIGHT_REST_LEGAL_HOLIDAY,
                 'double_holiday' => HourlyRateType::NIGHT_DOUBLE_HOLIDAY,
                 'double_holiday_and_rest_day' => HourlyRateType::NIGHT_REST_DOUBLE_HOLIDAY,
+                default => null,
+            };
+
+            return (object)[
+                'key' => $rate->key,
+                'hourly_rate_type' => $rate_type,
+                'value' => $rate->value,
+            ];
+        });
+    }
+
+    function getOvertimeRegularRates(): ?Collection
+    {
+        if(empty($this->companyOvertimeFormulaSettings)){
+            return null;
+        }
+
+        $companyOvertimeRegularRates = collect(
+            $this->companyOvertimeFormulaSettings
+                ->where('key', 'regular_rates')
+                ->first()
+                ->value
+        )->sortBy('order');
+
+        return $companyOvertimeRegularRates->map(function ($rate){
+
+            $rate_type = match($rate->key){
+                'regular' => HourlyRateType::OVERTIME_REGULAR,
+                'rest_day' => HourlyRateType::OVERTIME_REST,
+                'special_holiday' => HourlyRateType::OVERTIME_SPECIAL_HOLIDAY,
+                'special_holiday_and_rest_day' => HourlyRateType::OVERTIME_REST_SPECIAL_HOLIDAY,
+                'legal_holiday' => HourlyRateType::OVERTIME_LEGAL_HOLIDAY,
+                'legal_holiday_and_rest_day' => HourlyRateType::OVERTIME_REST_LEGAL_HOLIDAY,
+                'double_holiday' => HourlyRateType::OVERTIME_DOUBLE_HOLIDAY,
+                'double_holiday_and_rest_day' => HourlyRateType::OVERTIME_REST_DOUBLE_HOLIDAY,
+                default => null,
+            };
+
+            return (object)[
+                'key' => $rate->key,
+                'hourly_rate_type' => $rate_type,
+                'value' => $rate->value,
+            ];
+        });
+    }
+
+    function getOvertimeNightDifferentialRates(): ?Collection
+    {
+        if(empty($this->companyOvertimeFormulaSettings)){
+            return null;
+        }
+
+        $companyOvertimeNightDifferentialRates = collect(
+            $this->companyOvertimeFormulaSettings
+                ->where('key', 'night_differential_rates')
+                ->first()
+                ->value
+        )->sortBy('order');
+
+        return $companyOvertimeNightDifferentialRates->map(function ($rate){
+
+            $rate_type = match($rate->key){
+                'regular' => HourlyRateType::OVERTIME_NIGHT_REGULAR,
+                'rest_day' => HourlyRateType::OVERTIME_NIGHT_REST,
+                'special_holiday' => HourlyRateType::OVERTIME_NIGHT_SPECIAL_HOLIDAY,
+                'special_holiday_and_rest_day' => HourlyRateType::OVERTIME_NIGHT_REST_SPECIAL_HOLIDAY,
+                'legal_holiday' => HourlyRateType::OVERTIME_NIGHT_LEGAL_HOLIDAY,
+                'legal_holiday_and_rest_day' => HourlyRateType::OVERTIME_NIGHT_REST_LEGAL_HOLIDAY,
+                'double_holiday' => HourlyRateType::OVERTIME_NIGHT_DOUBLE_HOLIDAY,
+                'double_holiday_and_rest_day' => HourlyRateType::OVERTIME_NIGHT_REST_DOUBLE_HOLIDAY,
                 default => null,
             };
 
@@ -139,6 +250,7 @@ trait WorkPeriod
         $this->shiftWorkStartGraceTime = $this->shift->work_start_grace_time;
         $this->shiftRequireLunchOutAndIn = $this->shift->require_lunch_time_in_and_out;
         $this->shiftLunchStartGraceTime = $this->shift->lunch_start_grace_time;
+        $this->shiftOvertimeLimit = $this->shift->max_overtime;
     }
 
     /**
@@ -168,6 +280,7 @@ trait WorkPeriod
         $this->shiftWorkStartGraceTime = 0;
         $this->shiftRequireLunchOutAndIn = false;
         $this->shiftLunchStartGraceTime = 0;
+        $this->shiftOvertimeLimit = 0;
         $this->restDays = [];
         $this->schedules = [];
         $this->attendanceSchedule = null;
