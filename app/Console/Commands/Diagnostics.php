01705c4d56c494dfcde0b8b999fc381c40a8dd76
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-20 11:21:40 +0800

    Add AttendanceDetailsTests Class
    - Introduce `AttendanceDetailsTests` class for shift-based test cases.
    - Define various test scenarios such as regular, night, and flexible shifts.
    - Implement constructor to initialize test data and scenarios dynamically.
    - Add methods to handle validation logic for shifts with lunch breaks and flexible timings.
    - Provide test case configurations with expected outputs for comprehensive testing.

---
+++ app/Console/Commands/Diagnostics.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Concrete\Tests\AttendanceDetailsTests;
+use App\Models\Company;
+use Illuminate\Console\Command;
+
+class Diagnostics extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:diagnostics {company}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run test cases';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $company = Company::query()->findOrFail($this->argument('company'));
+
+        $this->line('Diagnostics command executed');
+        $this->newLine();
+        $this->line('Running tests');
+
+        $this->info('Attendance Splitter: ' . new AttendanceDetailsTests($company)->run());
+
+        $this->newLine();
+        $this->line('Done');
+    }
+}
