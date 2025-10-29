8fea5bad19e66996c6a76c3cb74f93442fcae1ea
Kim@PC <kimdeguzman20@yahoo.com>
2025-10-29 12:02:01 +0800

    Update Lunch Break Time Format in ShiftSchedule ListTransformer
    - Change `lunch_break_start` and `lunch_break_end` time format from 12-hour (`h:i A`) to 24-hour (`H:i`) for consistency.

--- app/Transformers/ShiftSchedule/ListTransformer.php
+++ app/Transformers/ShiftSchedule/ListTransformer.php
@@ -22,8 +22,8 @@ class ListTransformer extends TransformerAbstract
             'work_end' => $model->work_end ? Carbon::parse($model->work_end)->format('H:i') : $model->work_end,
             'total_work_hours_with_breaks' => $model->total_work_hours_with_breaks,
             'has_lunch_break' => (int)$model->has_lunch_break,
-            'lunch_break_start' => $model->lunch_break_start ? Carbon::parse($model->lunch_break_start)->format('h:i A') : $model->lunch_break_start,
-            'lunch_break_end' => $model->lunch_break_end ? Carbon::parse($model->lunch_break_end)->format('h:i A') : $model->lunch_break_end,
+            'lunch_break_start' => $model->lunch_break_start ? Carbon::parse($model->lunch_break_start)->format('H:i') : $model->lunch_break_start,
+            'lunch_break_end' => $model->lunch_break_end ? Carbon::parse($model->lunch_break_end)->format('H:i') : $model->lunch_break_end,
             'total_lunch_break_hours' => $model->total_lunch_break_hours,
         ];
     }
