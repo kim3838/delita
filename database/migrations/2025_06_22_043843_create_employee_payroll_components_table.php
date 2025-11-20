b339ff462f485896fef1d756968907e26e3ad523
Kim@PC <kimdeguzman20@yahoo.com>
2025-11-20 09:42:06 +0800

    Update Default Values for Amountable Start and End in Payroll Components Migration
    - Set default values for `amountable_start` and `amountable_end` columns using enumeration constants.
    - Imported `AmountablePayrollComponentStart` and `AmountablePayrollComponentEnd` enums into the migration file.

--- database/migrations/2025_06_22_043843_create_employee_payroll_components_table.php
+++ database/migrations/2025_06_22_043843_create_employee_payroll_components_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\AmountablePayrollComponentEnd;
+use App\Enums\AmountablePayrollComponentStart;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -22,9 +24,9 @@ return new class extends Migration
             $table->smallInteger('pay_period')->nullable();
             $table->smallInteger('pay_type')->nullable();
             $table->foreignId('pay_frequency_id')->nullable()->constrained('pay_frequencies')->onDelete('set null');
-            $table->smallInteger('amountable_start')->nullable();
+            $table->smallInteger('amountable_start')->default(AmountablePayrollComponentStart::NOT_SPECIFIED);
             $table->date('start_date')->nullable();
-            $table->smallInteger('amountable_end')->nullable();
+            $table->smallInteger('amountable_end')->default(AmountablePayrollComponentEnd::NOT_SPECIFIED);
             $table->date('end_date')->nullable();
             $table->timestamps();
 
