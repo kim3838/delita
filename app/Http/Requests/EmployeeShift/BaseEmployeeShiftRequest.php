d099040cf4e78f2f3f44ca1faabe2b6e36fcbd4d
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-06 10:41:44 +0800

    Update Validation Messages in BaseEmployeeShiftRequest
    - Reorganized validation messages for better structure and clarity.
    - Moved `employees` and `shifts` validation rules to ensure proper grouping.

--- app/Http/Requests/EmployeeShift/BaseEmployeeShiftRequest.php
+++ app/Http/Requests/EmployeeShift/BaseEmployeeShiftRequest.php
@@ -22,17 +22,17 @@ class BaseEmployeeShiftRequest extends FormRequest
     {
         return [
             'company_id.required' => 'Company is required',
-            'employees.required' => 'Employees is required',
-            'shifts.required' => 'Shifts is required',
             'start_date.required' => 'Shift start date is required',
             'end_date.required' => 'Shift end date is required if stated shift end date is true',
             'stated_shift_end_date.required' => 'Stated shift end date is required',
-            'employees.array' => 'Employees must be an array',
-            'shifts.array' => 'Shifts must be an array',
             'start_date.date_format' => 'Start date must match the format Y-m-d e.g.(2000-12-31)',
             'end_date.date_format' => 'End date must match the format Y-m-d e.g.(2000-12-31)',
             'stated_shift_end_date.boolean' => 'Stated shift end date must be a boolean',
             'end_date.after_or_equal' => 'End date must be equal to or after the start date',
+            'employees.required' => 'Employees is required',
+            'employees.array' => 'Employees must be an array',
+            'shifts.required' => 'Shifts is required',
+            'shifts.array' => 'Shifts must be an array',
         ];
     }
 }
