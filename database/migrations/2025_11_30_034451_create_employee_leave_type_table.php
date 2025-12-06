393370138949890d558bea0beb61c0de5ba15a04
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-06 11:00:27 +0800

    Make `balance_upon_eligibility` Nullable in Employee Leave Type Migration
    - Updated `balance_upon_eligibility` column to be nullable for greater flexibility in leave type configurations.
    - Ensured compatibility with existing `override_balance_upon_eligibility` logic.

--- database/migrations/2025_11_30_034451_create_employee_leave_type_table.php
+++ database/migrations/2025_11_30_034451_create_employee_leave_type_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             //Override Leave type initial eligibility balance
             $table->boolean('override_balance_upon_eligibility')->default(false);
-            $table->integer('balance_upon_eligibility')->default(0);
+            $table->integer('balance_upon_eligibility')->nullable();
             $table->timestamps();
         });
     }
