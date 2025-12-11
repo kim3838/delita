c520aaef7f0d2937935487349bfbda1245980313
Kim@PC <kimdeguzman20@yahoo.com>
2025-12-11 13:34:49 +0800

    Enhance Leave Eligibility and Period Calculation Logic
    - Added comprehensive eligibility determination logic for employee leave types using recursive and partitioned queries.
    - Refactored query builders for better readability and modularity.
    - Implemented handling of `eligibility_date_start` for leave period calculations.
    - Introduced calculations for `sequence_by_period_type` and `period` with varied span types.

--- app/Concrete/LeaveService.php
+++ app/Concrete/LeaveService.php
@@ -15,6 +15,16 @@ class LeaveService
     {
         $employee = clone $employeeLeaveType->employee;
         $leaveType = clone $employeeLeaveType->leaveType;
+        $dateSeriesPartition = false;
+        $dateSeriesPartitionColumn = DB::raw("
+            DENSE_RANK() OVER(
+                PARTITION BY employment_profile_by_date_series_sub.year
+                ORDER BY
+                    employment_profile_by_date_series_sub.year,
+                    employment_profile_by_date_series_sub.month,
+                    employment_profile_by_date_series_sub.employment_type
+            ) as `date_series_partition`
+        ");
 
         $employmentProfileSeries = "
             WITH RECURSIVE employment_profile_series(date_start,date_series) AS (
@@ -96,26 +106,21 @@ class LeaveService
                 DB::raw("employment_profile_by_date_series_sub.month"),
                 DB::raw("employment_profile_by_date_series_sub.date_start"),
                 DB::raw("employment_profile_by_date_series_sub.date_series"),
-                DB::raw("DENSE_RANK() OVER(
-                    PARTITION BY employment_profile_by_date_series_sub.year
-                    ORDER BY
-                        employment_profile_by_date_series_sub.year,
-                        employment_profile_by_date_series_sub.month,
-                        employment_profile_by_date_series_sub.employment_type
-                ) as `date_series_partition`"),
+                //Date series partition slot
                 DB::raw("employment_profile_by_date_series_sub.employment_type"),
             ]);
 
-        $employeeLeaveTypeSequenceByPeriodQueryBuilder = app(QueryBuilder::class)
-            ->fromSub($employmentProfileByDateSeriesQueryBuilder, 'employment_profile_by_date_series')
+        $employeeLeaveTypeEligibilityQueryBuilder = app(QueryBuilder::class)
+            ->fromSub($employmentProfileByDateSeriesQueryBuilder, 'epbds')
             ->join('employee_leave_type', function ($join) use($employeeLeaveType) {
                 $join->where('employee_leave_type.leave_type_id', $employeeLeaveType->leave_type_id)
                     ->where('employee_leave_type.employee_id', $employeeLeaveType->employee_id);
             })
             ->leftJoin('leave_types', 'leave_types.id', '=', 'employee_leave_type.leave_type_id')
             ->select([
-                DB::raw("employment_profile_by_date_series.*"),
+                DB::raw("epbds.*"),
                 DB::raw("leave_types.period_type"),
+                DB::raw("leave_types.period_interval_span_type"),
                 DB::raw("
                     CASE
                         WHEN leave_types.period_type = ".LeavePeriodType::INTERVAL->value." AND (leave_types.period_interval_span_type = ".LeaveIntervalSpanType::DAY->value." OR leave_types.period_interval_span_type = ".LeaveIntervalSpanType::MONTH->value.")
@@ -126,19 +131,79 @@ class LeaveService
                             THEN leave_types.period_calendar_span_value
                     END AS `period_span_value`
                 "),
+                DB::raw("COALESCE(JSON_CONTAINS(leave_types.eligibility_employment_types, CAST(epbds.employment_type AS CHAR)),0) AS `eligible`")
+            ]);
+
+        $employeeLeaveTypeSequenceStartQueryBuilder = app(QueryBuilder::class)
+            ->fromSub($employeeLeaveTypeEligibilityQueryBuilder, 'elte')
+            ->select([
+                DB::raw("elte.*"),
+                DB::raw("SUM(eligible) OVER(ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0 AS `eligibility_started`"),
+            ]);
+
+        $employeeLeaveTypeEligibilityDateStartReferenceQueryBuilder = app(QueryBuilder::class)
+            ->fromSub($employeeLeaveTypeSequenceStartQueryBuilder, 'eltss')
+            ->select([
+                DB::raw("eltss.*"),
+                DB::raw("
+                    CASE
+                        WHEN eltss.eligibility_started = 1 AND LAG(eltss.eligibility_started) OVER(ORDER BY eltss.date_series) IS NULL
+                        THEN eltss.date_start
+                        WHEN eltss.eligibility_started > 0 AND LAG(eltss.eligibility_started) OVER(ORDER BY eltss.date_series) = 0
+                        THEN eltss.date_series
+                    END AS `eligibility_date_start_reference`
+                "),
+            ]);
+
+        $employeeLeaveTypeEligibilityDateStartQueryBuilder = app(QueryBuilder::class)
+            ->fromSub($employeeLeaveTypeEligibilityDateStartReferenceQueryBuilder, 'eltsr')
+            ->select([
+                DB::raw("eltsr.*"),
+                DB::raw("
+                    CASE
+                        WHEN eltsr.eligibility_started > 0
+                        THEN MAX(eltsr.eligibility_date_start_reference) OVER(ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
+                    END AS `eligibility_date_start`
+                "),
+            ]);
+
+        $employeeLeaveTypeSequenceByPeriodQueryBuilder = app(QueryBuilder::class)
+            ->fromSub($employeeLeaveTypeEligibilityDateStartQueryBuilder, 'elteds')
+            ->select([
+                DB::raw("elteds.*"),
                 DB::raw("
                     CASE
                         WHEN
-                            (leave_types.period_type = ".LeavePeriodType::INTERVAL->value." AND (leave_types.period_interval_span_type = ".LeaveIntervalSpanType::MONTH->value." OR leave_types.period_interval_span_type = ".LeaveIntervalSpanType::YEAR->value."))
-                            OR leave_types.period_type = ".LeavePeriodType::CALENDAR_YEAR->value."
-                            THEN 1+PERIOD_DIFF(
-                                CONCAT(YEAR(employment_profile_by_date_series.date_series),LPAD(MONTH(employment_profile_by_date_series.date_series), 2, '0')),
-                                CONCAT(YEAR(employment_profile_by_date_series.date_start),LPAD(MONTH(employment_profile_by_date_series.date_start), 2, '0'))
+                            (elteds.period_type = ".LeavePeriodType::INTERVAL->value." AND (elteds.period_interval_span_type = ".LeaveIntervalSpanType::MONTH->value." OR elteds.period_interval_span_type = ".LeaveIntervalSpanType::YEAR->value."))
+                            OR elteds.period_type = ".LeavePeriodType::CALENDAR_YEAR->value."
+                        THEN 1+PERIOD_DIFF(
+                                CONCAT(YEAR(elteds.date_series),LPAD(MONTH(elteds.date_series), 2, '0')),
+                                CONCAT(YEAR(elteds.eligibility_date_start),LPAD(MONTH(elteds.eligibility_date_start), 2, '0'))
                             )
-                        WHEN leave_types.period_type = ".LeavePeriodType::INTERVAL->value." AND leave_types.period_interval_span_type = ".LeaveIntervalSpanType::DAY->value."
-                            THEN 1+DATEDIFF(employment_profile_by_date_series.date_series,employment_profile_by_date_series.date_start)
+                        WHEN elteds.period_type = ".LeavePeriodType::INTERVAL->value." AND elteds.period_interval_span_type = ".LeaveIntervalSpanType::DAY->value."
+                        THEN 1+DATEDIFF(elteds.date_series,elteds.eligibility_date_start)
                     END AS `sequence_by_period_type`
-                ")
+                "),
+            ]);
+
+        $employeeLeaveTypeByPeriodQueryBuilder = app(QueryBuilder::class)
+            ->fromSub($employeeLeaveTypeSequenceByPeriodQueryBuilder, 'eltsbp')
+            ->select([
+                DB::raw("eltsbp.*"),
+                DB::raw("
+                    CASE WHEN eltsbp.eligibility_started > 0 THEN
+                        CASE
+                            WHEN eltsbp.period_type = ".LeavePeriodType::INTERVAL->value."
+                                THEN
+                                CEIL(eltsbp.sequence_by_period_type / eltsbp.period_span_value)
+                            WHEN eltsbp.period_type = ".LeavePeriodType::CALENDAR_YEAR->value."
+                                THEN
+                                    (IF(MONTH(eltsbp.eligibility_date_start) < eltsbp.period_span_value, 1, 0))
+                                    +SUM(IF(CONCAT(YEAR(eltsbp.date_series),LPAD(MONTH(eltsbp.date_series), 2, '0'),LPAD(DAY(eltsbp.date_series), 2, '0')) = CONCAT(eltsbp.year, LPAD(eltsbp.period_span_value, 2, '0'), '01'), 1, 0)
+                                ) OVER(ORDER BY eltsbp.year,eltsbp.month)
+                        END
+                    END AS `period`
+                "),
             ]);
     }
 }
