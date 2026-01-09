8cced5aef76aa5cb3183b4c53ef85d9e784a53f1
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-09 10:04:02 +0800

    Add Permission Check Method to BasePolicy
    - Added `hasPermission` method to validate user permissions based on roles and account ID.
    - Enhanced reusability and centralized permission logic within the BasePolicy class.

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -30,4 +30,20 @@ class BasePolicy
             ->where('assignment_type', CompanyUserAssignmentType::ADMIN->value)
             ->count();
     }
+
+    protected function hasPermission(User $user, string $permission, $accountId = null): bool
+    {
+        $permitted = false;
+        $accountId = empty($accountId) ? request()->input('account_id') : $accountId;
+        $userRoles = $user->roles->where('account_id', $accountId);
+
+        foreach ($userRoles as $role){
+
+            $permitted = $role->hasPermissionTo($permission);
+
+            if($permitted) break;
+        }
+
+        return $permitted;
+    }
 }
