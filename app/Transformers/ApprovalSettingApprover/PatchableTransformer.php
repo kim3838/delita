7a219e784e199dcbc4a3bbba3f27965c6cb3efab
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-15 08:55:19 +0800

    Refactor `ApprovalSettingApprover/PatchableTransformer` for Enhanced Company Mapping
    - Replaced `Employee` model usage with `User/ListTransformer` for associated company mapping.
    - Added new attributes: `company_id`, `company_name`, `is_employee` to the response structure.
    - Removed `mapAssociatedCompanies` method, delegating the responsibility to `User/ListTransformer` for simplified and centralized logic.

--- app/Transformers/ApprovalSettingApprover/PatchableTransformer.php
+++ app/Transformers/ApprovalSettingApprover/PatchableTransformer.php
@@ -5,7 +5,7 @@ namespace App\Transformers\ApprovalSettingApprover;
 use App\Concrete\TransformerAbstractConcrete;
 use App\Enums\CompanyUserAssignmentType;
 use App\Models\ApprovalSettingApprover;
-use App\Models\Employee;
+use App\Transformers\User\ListTransformer as UserListTransformer;
 
 class PatchableTransformer extends TransformerAbstractConcrete
 {
@@ -19,25 +19,33 @@ class PatchableTransformer extends TransformerAbstractConcrete
             ? $user->companies->whereIn('id', $filters->company_id)->values()
             : $user->companies;
 
-        $mappedAssociatedCompanies = collect($this->mapAssociatedCompanies($associatedCompanies));
+        $mappedAssociatedCompanies = collect(new UserListTransformer()->mapAssociatedCompanies($associatedCompanies));
 
         $accountRoles = request()->account_id
             ? $this->collectionSummary($user->roles->where('account_id', request()->account_id)->values(), 'name', '')
             : null;
 
-        $companyEmployeeNumber = '';
+        $companyId = '';
+        $companyName = '';
         $companyEmployeeAssignmentType = '';
+        $isEmployee = false;
+        $companyEmployeeNumber = '';
         $companyEmployeeFullName = '';
 
         if($mappedAssociatedCompanies->first()){
 
-            if($mappedAssociatedCompanies->first()['is_employee']){
+            $companyId = $mappedAssociatedCompanies->first()['id'];
+            $companyName = $mappedAssociatedCompanies->first()['name'];
+
+            $isEmployee = $mappedAssociatedCompanies->first()['is_employee'];
+
+            if($isEmployee){
                 $companyEmployeeNumber = $mappedAssociatedCompanies->first()['employee_number'];
                 $companyEmployeeFullName = $mappedAssociatedCompanies->first()['employee_full_name'];
             }
 
             $companyEmployeeAssignmentType = $mappedAssociatedCompanies->first()['assignment']['value'] == CompanyUserAssignmentType::ADMIN->value
-                ? $mappedAssociatedCompanies->first()['assignment']['text']
+                ? $mappedAssociatedCompanies->first()['assignment']
                 : '';
         }
 
@@ -47,30 +55,15 @@ class PatchableTransformer extends TransformerAbstractConcrete
             'order' => $model->order,
             'approver_id' => $model->approver_id,
             'approver_username' => $model->approver->name,
+
+            'company_id' => $companyId,
+            'company_name' => $companyName,
+            'company_assignment_type' => $companyEmployeeAssignmentType,
+            'is_employee' => $isEmployee,
             'company_employee_number' => $companyEmployeeNumber,
             'company_employee_full_name' => $companyEmployeeFullName,
-            'company_assignment_type' => $companyEmployeeAssignmentType,
 
             'account_roles_summary' => $accountRoles,
         ];
-    }
-
-    public function mapAssociatedCompanies($associatedCompanies): array
-    {
-        return $associatedCompanies
-            ->map(function($assignedCompany){
-
-                $employee = Employee::query()
-                    ->where('user_id', $assignedCompany->pivot->user_id)
-                    ->where('company_id', $assignedCompany->id)
-                    ->first();
-
-                return [
-                    'assignment' => CompanyUserAssignmentType::tryFrom($assignedCompany->pivot->assignment_type)?->toArray() ?? null,
-                    'is_employee' => (bool)$employee,
-                    'employee_number' => $employee?->number,
-                    'employee_full_name' => $employee?->full_name,
-                ];
-            })->toArray();
     }
 }
