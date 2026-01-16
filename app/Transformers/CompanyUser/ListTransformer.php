ce2e4f0431263ccd09528684863e5be6ae42fee8
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-16 16:08:33 +0800

    Add `ListTransformer` for Company User Data Transformation
    - Created `ListTransformer` in `CompanyUser` namespace for structuring user listing response data.
    - Introduced detailed transformation logic for company user attributes.
    - Included support for summarizing user account roles based on request account ID.

---
+++ app/Transformers/CompanyUser/ListTransformer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Transformers\CompanyUser;
+
+use App\Concrete\TransformerAbstractConcrete;
+use App\Models\Hydrations\CompanyUser;
+use App\Models\User;
+
+class ListTransformer extends TransformerAbstractConcrete
+{
+    public function transform(CompanyUser $model): array
+    {
+        $user = User::query()->find($model->user_id);
+
+        $accountRoles = request()->account_id
+            ? $this->collectionSummary($user->roles->where('account_id', request()->account_id)->values(), 'name', '')
+            : null;
+
+        return [
+            'company_id' => $model->company_id,
+            'company_name' => $model->company_name,
+            'company_assignment_type' => $model->company_assignment_type?->toArray(),
+            'is_employee' => $model->is_employee,
+            'company_employee_number' => $model->company_employee_number,
+            'company_employee_full_name' => implode(' ', array_filter([
+                $model->company_employee_family_name,
+                $model->company_employee_given_name,
+                $model->company_employee_middle_name
+            ])),
+
+            'id' => $model->user_id,
+            'ulid' => $model->user_ulid,
+            'username' => $model->user_username,
+            'email' => $model->user_email,
+            'status' => $model->user_status?->toArray(),
+            'email_verified_at' => $model->user_email_verified_at,
+            'timezone' => $model->user_timezone,
+
+            'account_roles_summary' => $accountRoles,
+        ];
+    }
+}
