22cbed2305d396df454587e73a879c5890337549
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-19 14:55:25 +0800

    Add `Number` Column to Attendance Adjustment Requests Table
    - Introduced `number` string column in `attendance_adjustment_requests` table schema to support custom numbering functionality.

--- database/migrations/2026_01_13_090235_create_attendance_adjustment_requests_table.php
+++ database/migrations/2026_01_13_090235_create_attendance_adjustment_requests_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('attendance_adjustment_requests', function (Blueprint $table) {
             $table->id();
+            $table->string('number');
             $table->foreignId('company_id')->constrained('companies')->cascadeOnDelete();
             $table->foreignId('requested_by')->constrained('users')->cascadeOnDelete();
             $table->dateTime('date_requested');
