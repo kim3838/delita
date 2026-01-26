1283d5361d6b631f698fc1c1158287056d86e8d6
Kim@PC <kimdeguzman20@yahoo.com>
2026-01-26 12:36:08 +0800

    Add Leave Requests to User Filed Request Query
    - Integrated `LeaveRequestRepository` into `UserFiledRequestRepositoryEloquent`.
    - Updated query to include leave requests alongside attendance adjustments and overtime requests.
    - Added logic in `RequestApprovalStateRepositoryEloquent` to support `leave_requests` data handling.

--- app/Concrete/Repositories/RequestApprovalStateRepositoryEloquent.php
+++ app/Concrete/Repositories/RequestApprovalStateRepositoryEloquent.php
@@ -54,6 +54,8 @@ class RequestApprovalStateRepositoryEloquent extends BaseRepositoryEloquent impl
                         (SELECT companies.account_id FROM attendance_adjustment_requests LEFT JOIN companies ON companies.id = attendance_adjustment_requests.company_id WHERE attendance_adjustment_requests.id = requestable_id)
                     WHEN requestable_type = 'overtime_request' THEN
                         (SELECT companies.account_id FROM overtime_requests LEFT JOIN companies ON companies.id = overtime_requests.company_id WHERE overtime_requests.id = requestable_id)
+                    WHEN requestable_type = 'leave_request' THEN
+                        (SELECT companies.account_id FROM leave_requests LEFT JOIN companies ON companies.id = leave_requests.company_id WHERE leave_requests.id = requestable_id)
                     ELSE '' END AS requestable_account_id
                 "),
                 DB::raw("
@@ -61,6 +63,8 @@ class RequestApprovalStateRepositoryEloquent extends BaseRepositoryEloquent impl
                         (SELECT company_id FROM attendance_adjustment_requests WHERE id = requestable_id)
                     WHEN requestable_type = 'overtime_request' THEN
                         (SELECT company_id FROM overtime_requests WHERE id = requestable_id)
+                    WHEN requestable_type = 'leave_request' THEN
+                        (SELECT company_id FROM leave_requests WHERE id = requestable_id)
                     ELSE '' END AS requestable_company_id
                 "),
                 DB::raw("
@@ -68,6 +72,8 @@ class RequestApprovalStateRepositoryEloquent extends BaseRepositoryEloquent impl
                         (SELECT number FROM attendance_adjustment_requests WHERE id = requestable_id)
                     WHEN requestable_type = 'overtime_request' THEN
                         (SELECT number FROM overtime_requests WHERE id = requestable_id)
+                    WHEN requestable_type = 'leave_request' THEN
+                        (SELECT number FROM leave_requests WHERE id = requestable_id)
                     ELSE '' END AS requestable_number
                 "),
                 DB::raw("
@@ -75,6 +81,8 @@ class RequestApprovalStateRepositoryEloquent extends BaseRepositoryEloquent impl
                         (SELECT date_requested FROM attendance_adjustment_requests WHERE id = requestable_id)
                     WHEN requestable_type = 'overtime_request' THEN
                         (SELECT date_requested FROM overtime_requests WHERE id = requestable_id)
+                    WHEN requestable_type = 'leave_request' THEN
+                        (SELECT date_requested FROM leave_requests WHERE id = requestable_id)
                     ELSE '' END AS requestable_date_requested
                 "),
                 DB::raw("request_approval_states.order"),
--- app/Concrete/Repositories/UserFiledRequestRepositoryEloquent.php
+++ app/Concrete/Repositories/UserFiledRequestRepositoryEloquent.php
@@ -4,6 +4,7 @@ namespace App\Concrete\Repositories;
 
 use App\Blueprint\Repositories\AttendanceAdjustmentRequestRepository;
 use App\Blueprint\Repositories\CompanyUserRepository;
+use App\Blueprint\Repositories\LeaveRequestRepository;
 use App\Blueprint\Repositories\OvertimeRequestRepository;
 use App\Blueprint\Repositories\UserFiledRequestRepository;
 use App\Concrete\BaseRepositoryEloquent;
@@ -34,6 +35,9 @@ class UserFiledRequestRepositoryEloquent extends BaseRepositoryEloquent implemen
 
         $requestableFilter = clone $filters;
 
+        /**
+         * All user attendance adjustment requests
+         * */
         $attendanceAdjustmentRequestBuilder = App::make(AttendanceAdjustmentRequestRepository::class)->baseQueryBuilder($requestableFilter, []);
 
         $attendanceAdjustmentRequestBuilder = $this->queryAsSub($companyUserQueryBuilder, 'company_user_sub')
@@ -70,6 +74,38 @@ class UserFiledRequestRepositoryEloquent extends BaseRepositoryEloquent implemen
                 'attendance_adjustment_request_sub.status_summary AS status_summary',
             ]);
 
+        $attendanceAdjustmentRequestBuilder = $this->queryAsSub($attendanceAdjustmentRequestBuilder, 'attendance_adjustment_requests')
+            ->select([
+                DB::raw("CONCAT(attendance_adjustment_requests.requestable_type, '_', attendance_adjustment_requests.requestable_id) AS id"),
+                'attendance_adjustment_requests.requestable_type',
+                'attendance_adjustment_requests.requestable_id',
+                'attendance_adjustment_requests.number',
+                'attendance_adjustment_requests.date_requested',
+                'attendance_adjustment_requests.remarks',
+                'attendance_adjustment_requests.status_summary',
+
+                'attendance_adjustment_requests.user_id',
+                'attendance_adjustment_requests.user_ulid',
+                'attendance_adjustment_requests.user_username',
+                'attendance_adjustment_requests.user_email',
+                'attendance_adjustment_requests.user_status',
+                'attendance_adjustment_requests.user_email_verified_at',
+                'attendance_adjustment_requests.user_timezone',
+
+                'attendance_adjustment_requests.company_id AS user_company_id',
+                'attendance_adjustment_requests.company_name',
+                'attendance_adjustment_requests.company_timezone',
+                'attendance_adjustment_requests.company_assignment_type',
+                'attendance_adjustment_requests.is_employee',
+                'attendance_adjustment_requests.company_employee_number',
+                'attendance_adjustment_requests.company_employee_family_name',
+                'attendance_adjustment_requests.company_employee_middle_name',
+                'attendance_adjustment_requests.company_employee_given_name',
+            ]);
+
+        /**
+         * All user overtime requests
+         * */
         $overtimeRequestBuilder = App::make(OvertimeRequestRepository::class)->baseQueryBuilder($requestableFilter, []);
 
         $overtimeRequestBuilder = $this->queryAsSub($companyUserQueryBuilder, 'company_user_sub')
@@ -135,37 +171,80 @@ class UserFiledRequestRepositoryEloquent extends BaseRepositoryEloquent implemen
                 'overtime_requests.company_employee_given_name',
             ]);
 
-        $attendanceAdjustmentRequestBuilder = $this->queryAsSub($attendanceAdjustmentRequestBuilder, 'attendance_adjustment_requests')
+        /**
+         * All user leave requests
+         * */
+        $leaveRequestBuilder = App::make(LeaveRequestRepository::class)->baseQueryBuilder($requestableFilter, []);
+
+        $leaveRequestBuilder = $this->queryAsSub($companyUserQueryBuilder, 'company_user_sub')
+            ->joinSub($leaveRequestBuilder, 'leave_request_sub', function ($join) {
+                $join->on('leave_request_sub.requested_by', '=', 'company_user_sub.user_id');
+            })
+            ->where(function ($clause) use ($filters) {
+                $clause->whereIn('leave_request_sub.company_id', $filters->associated_companies);
+            })
             ->select([
-                DB::raw("CONCAT(attendance_adjustment_requests.requestable_type, '_', attendance_adjustment_requests.requestable_id) AS id"),
-                'attendance_adjustment_requests.requestable_type',
-                'attendance_adjustment_requests.requestable_id',
-                'attendance_adjustment_requests.number',
-                'attendance_adjustment_requests.date_requested',
-                'attendance_adjustment_requests.remarks',
-                'attendance_adjustment_requests.status_summary',
+                'company_user_sub.user_id',
+                'company_user_sub.user_ulid',
+                'company_user_sub.user_username',
+                'company_user_sub.user_email',
+                'company_user_sub.user_status',
+                'company_user_sub.user_email_verified_at',
+                'company_user_sub.user_timezone',
+                'company_user_sub.company_id AS user_company_id',
+                'company_user_sub.company_name',
+                'company_user_sub.company_timezone',
+                'company_user_sub.company_assignment_type',
+                'company_user_sub.is_employee',
+                'company_user_sub.company_employee_number',
+                'company_user_sub.company_employee_family_name',
+                'company_user_sub.company_employee_middle_name',
+                'company_user_sub.company_employee_given_name',
 
-                'attendance_adjustment_requests.user_id',
-                'attendance_adjustment_requests.user_ulid',
-                'attendance_adjustment_requests.user_username',
-                'attendance_adjustment_requests.user_email',
-                'attendance_adjustment_requests.user_status',
-                'attendance_adjustment_requests.user_email_verified_at',
-                'attendance_adjustment_requests.user_timezone',
-
-                'attendance_adjustment_requests.company_id AS user_company_id',
-                'attendance_adjustment_requests.company_name',
-                'attendance_adjustment_requests.company_timezone',
-                'attendance_adjustment_requests.company_assignment_type',
-                'attendance_adjustment_requests.is_employee',
-                'attendance_adjustment_requests.company_employee_number',
-                'attendance_adjustment_requests.company_employee_family_name',
-                'attendance_adjustment_requests.company_employee_middle_name',
-                'attendance_adjustment_requests.company_employee_given_name',
+                'leave_request_sub.company_id AS company_id',
+                DB::raw("'leave_request' AS requestable_type"),
+                'leave_request_sub.id AS requestable_id',
+                'leave_request_sub.number AS number',
+                'leave_request_sub.date_requested AS date_requested',
+                'leave_request_sub.remarks AS remarks',
+                'leave_request_sub.status_summary AS status_summary',
             ]);
 
+        $leaveRequestBuilder = $this->queryAsSub($leaveRequestBuilder, 'leave_requests')
+            ->select([
+                DB::raw("CONCAT(leave_requests.requestable_type, '_', leave_requests.requestable_id) AS id"),
+                'leave_requests.requestable_type',
+                'leave_requests.requestable_id',
+                'leave_requests.number',
+                'leave_requests.date_requested',
+                'leave_requests.remarks',
+                'leave_requests.status_summary',
+
+                'leave_requests.user_id',
+                'leave_requests.user_ulid',
+                'leave_requests.user_username',
+                'leave_requests.user_email',
+                'leave_requests.user_status',
+                'leave_requests.user_email_verified_at',
+                'leave_requests.user_timezone',
+                'leave_requests.company_id AS user_company_id',
+                'leave_requests.company_name',
+                'leave_requests.company_timezone',
+
+                'leave_requests.company_assignment_type',
+                'leave_requests.is_employee',
+                'leave_requests.company_employee_number',
+                'leave_requests.company_employee_family_name',
+                'leave_requests.company_employee_middle_name',
+                'leave_requests.company_employee_given_name',
+            ]);
+
+        /**
+         * All user requests
+         * */
         $queryBuilder = $attendanceAdjustmentRequestBuilder
-            ->unionAll($overtimeRequestBuilder);
+            ->unionAll($overtimeRequestBuilder)
+            ->unionAll($leaveRequestBuilder);
 
         $queryBuilder = $this->queryAsSub($queryBuilder, 'user_filed_requests')
             ->select([
