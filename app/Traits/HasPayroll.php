c344c18da5e5b39b93748b04f7cf7b074e294221
Kim@PC <kimdeguzman20@yahoo.com>
2026-02-04 07:44:03 +0800

    Remove Unused Payroll Transformation Logic in HasPayroll Trait
    - Deleted `transformPayrollPayload` method to clean up outdated functionality.
    - Removed `Carbon` import as it is no longer required.

--- app/Traits/HasPayroll.php
+++ app/Traits/HasPayroll.php
@@ -2,23 +2,7 @@
 
 namespace App\Traits;
 
-use Carbon\Carbon;
-
 trait HasPayroll
 {
-    public function transformPayrollPayload($payload): array
-    {
-        return [
-            'year' => $payload['year'],
-            'month' => $payload['month'],
-            'month_readable' => Carbon::createFromDate(null, $payload['month'], 1)->format('F'),
-            'pay_frequency' => $payload['pay_frequency'],
-            'pay_frequency_readable' => $payload['pay_frequency']?->label(),
-            'frequency_sequence' => $payload['frequency_sequence'],
-            'frequency_sequence_readable' => $payload['frequency_sequence']?->label(),
-            'start' => $payload['start']?->toDateString(),
-            'end' => $payload['end']?->toDateString(),
-            'date_range_readable' => $payload['start']->format('F j, Y') . ' - ' . $payload['end']->format('F j, Y'),
-        ];
-    }
+
 }
