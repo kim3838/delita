104a085034c7b55f51b0bf5a8580f248b8520393
Kim@PC <kimdeguzman20@yahoo.com>
2026-02-03 15:25:08 +0800

    Remove Unused Imports in PayrollInquiryRequest
    - Deleted `PayFrequency` and `Rule` imports from `PayrollInquiryRequest` for cleaner code.

--- app/Http/Requests/PayrollInquiry/PayrollInquiryRequest.php
+++ app/Http/Requests/PayrollInquiry/PayrollInquiryRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests\PayrollInquiry;
 
-use App\Enums\PayFrequency;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class PayrollInquiryRequest extends FormRequest
 {
