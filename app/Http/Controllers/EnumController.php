3a19d28223284b43b2db5540350805cafe6a86cc
Kim@PC <kimdeguzman20@yahoo.com>
2026-02-13 09:32:26 +0800

    Filter Compensation Selection by Assignable Status
    - Added filtering logic to exclude global compensations (e.g., leave pay, holiday pay) from the selection list when `assignable_only` is enabled.
    - Allowed support for dynamic filters via JSON input in the `selection` method.
    - Updated response to include applied filters for better traceability.

--- app/Http/Controllers/EnumController.php
+++ app/Http/Controllers/EnumController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Blueprint\EnumInterface;
+use App\Enums\Compensation as CompensationEnum;
 use App\Facades\ResponseJson;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\App;
@@ -11,10 +12,35 @@ class EnumController extends Controller
 {
     public function selection($enum): JsonResponse
     {
-        $enum = App::make(EnumInterface::class)->selection($enum);
+        $selection = [];
+        $filters = json_decode(request()->get('filters'));
+
+        switch($enum){
+            case 'compensation':
+
+                $assignableOnly = $filters->assignable_only ?? true;
+
+                $compensations = App::make(EnumInterface::class)->selection('compensation');
+
+                if($assignableOnly){
+                    $globalCompensations = [CompensationEnum::LEAVE_PAY->value, CompensationEnum::HOLIDAY_PAY->value];
+
+                    $selection = collect($compensations::all())->filter(function($compensation) use($globalCompensations){
+                        return !in_array($compensation['value'], $globalCompensations);
+                    })->values()->toArray();
+                } else {
+                    $selection = $compensations::all();
+                }
+
+
+                break;
+
+            default: $selection = App::make(EnumInterface::class)->selection($enum)::all();
+        }
 
         return ResponseJson::successfulResponse([
-            'data' => $enum::all()
+            'data' => $selection,
+            'filters' => $filters,
         ]);
     }
 
