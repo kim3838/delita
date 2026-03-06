8fab7ed469e5274aff1a2c107aac69bc2259eec6
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-06 14:33:27 +0800

    Update Date Formatting in Employment Profile Transformers
    - Changed readable date format from `F j, Y` to `M j, Y` in `ListTransformer` for consistency with other transformers.

--- app/Transformers/EmploymentProfile/ListTransformer.php
+++ app/Transformers/EmploymentProfile/ListTransformer.php
@@ -16,10 +16,10 @@ class ListTransformer extends TransformerAbstract
             'status' => $model->status?->toArray(),
             'employment_type' => $model->employment_type?->toArray(),
             'start_date' => $model->start_date?->format('Y-m-d'),
-            'start_date_readable' => $model->start_date?->format('F j, Y'),
+            'start_date_readable' => $model->start_date?->format('M j, Y'),
             'end_of_service_type' => $model->end_of_service_type?->toArray(),
             'end_date' => $model->end_date?->format('Y-m-d'),
-            'end_date_readable' => $model->end_date?->format('F j, Y'),
+            'end_date_readable' => $model->end_date?->format('M j, Y'),
             'created_at' => $model->created_at?->format('Y-m-d'),
             'employee' => [
                 'ulid' => $model->employee->ulid,
