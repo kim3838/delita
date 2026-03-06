2e969be3044568f77095aa92df518226b362adcc
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-06 14:33:22 +0800

    Add Export Policy for Salary Statements
    - Introduced `export` method in `SalaryStatementPolicy` to manage user permissions for exporting salary statements.
    - Enforced checks for super admin status or admin permissions with required `export-salary-statement` access.

--- app/Policies/SalaryStatementPolicy.php
+++ app/Policies/SalaryStatementPolicy.php
@@ -17,6 +17,16 @@ class SalaryStatementPolicy extends BasePolicy
             && $this->hasPermission($user, 'view-salary-statement');
     }
 
+    public function export(User $user): bool
+    {
+        if($user->isSuperAdmin()){
+            return true;
+        }
+
+        return $this->userIsAdminInCompany($user, request()->input('company_id'))
+            && $this->hasPermission($user, 'export-salary-statement');
+    }
+
     public function view(User $user, SalaryStatement $salaryStatement): bool
     {
         if($user->isSuperAdmin()){
