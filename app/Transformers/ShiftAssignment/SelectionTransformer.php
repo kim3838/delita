3c2732f89bee9c3672334dc74514a28e06b27c3b
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-09 19:53:21 +0800

    Update SelectionTransformer for Consistent Readable Date Formats
    - Changed date format in `SelectionTransformer` to use `M j, Y` for enhanced readability.
    - Updated logic for both ranged and "onwards" date cases to reflect the new format.

--- app/Transformers/ShiftAssignment/SelectionTransformer.php
+++ app/Transformers/ShiftAssignment/SelectionTransformer.php
@@ -10,8 +10,8 @@ class SelectionTransformer extends TransformerAbstract
     public function transform(ShiftAssignment $shiftAssignment): array
     {
         $assignmentReadable = $shiftAssignment->shift_stated_shift_end_date
-            ? $shiftAssignment->shift_start_date?->format('Y-m-d') . " to " . $shiftAssignment->shift_end_date?->format('Y-m-d')
-            : $shiftAssignment->shift_start_date?->format('Y-m-d') . " onwards.";
+            ? $shiftAssignment->shift_start_date?->format('M j, Y') . " to " . $shiftAssignment->shift_end_date?->format('M j, Y')
+            : $shiftAssignment->shift_start_date?->format('M j, Y') . " onwards.";
 
         return [
             'value' => $shiftAssignment->shift_id,
