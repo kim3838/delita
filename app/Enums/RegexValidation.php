9252af045ef5207a6eb0394042b34ce4da378409
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-14 09:12:09 +0800

    Add New Regex for 12-Digit Numbers with 2 Decimals
    - Introduced `NUMERIC_12_DIGITS_2_DECIMALS` validation regex in `RegexValidation` enum.

--- app/Enums/RegexValidation.php
+++ app/Enums/RegexValidation.php
@@ -8,5 +8,7 @@ enum RegexValidation: string
 
     case NUMERIC_12_DIGITS_6_DECIMALS = '/^\d{1,12}(\.\d{1,6})?$/';
 
+    case NUMERIC_12_DIGITS_2_DECIMALS = '/^\d{1,12}(\.\d{1,2})?$/';
+
     case NUMERIC_1_DECIMAL = '/^\d+(\.\d{1})?$/';
 }
