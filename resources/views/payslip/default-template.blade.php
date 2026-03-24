a227a32167524ea423600005c31159c31f2bba83
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-25 07:00:21 +0800

    Update Payslip Template Fonts and Styles
    - Replaced multiple font imports with a single `Google Sans` font for consistency and performance.
    - Adjusted font-family assignments to use `Google Sans` across the template.
    - Updated text color to pure black for better readability.
    - Removed unused `text-condensed` class for cleaner markup.

--- resources/views/payslip/default-template.blade.php
+++ resources/views/payslip/default-template.blade.php
@@ -7,10 +7,8 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap" rel="stylesheet">
+
 
     <style>
         html, body {
@@ -26,15 +24,17 @@
         }
 
         .font {
-            font-family: "Inter", sans-serif;
+            font-family: "Google Sans", sans-serif;
             font-optical-sizing: auto;
             font-style: normal;
+            font-variation-settings: "GRAD" 0;
         }
 
         .font-numeric {
-            font-family: "Funnel Sans", monospace;
+            font-family: "Google Sans", sans-serif;
             font-optical-sizing: auto;
             font-style: normal;
+            font-variation-settings: "GRAD" 0;
         }
 
         .payslip-body {
@@ -43,7 +43,7 @@
         }
 
         .text-color {
-            color: #1c1c1c;
+            color: #000000;
         }
 
         .sub-text-color {
@@ -250,7 +250,7 @@
                     </div>
                     @foreach($earning['payroll_item_sub_values'] as $earningSubValue)
                     <div class="flex-auto">
-                        <div class="text-condensed text-xs sub-text-color">{{$earningSubValue['label']}}</div>
+                        <div class="text-xs sub-text-color">{{$earningSubValue['label']}}</div>
                         <div class="text-xs font-numeric">{{$earningSubValue['value']}}</div>
                     </div>
                     @endforeach
@@ -276,7 +276,7 @@
                         </div>
                         @foreach($deduction['payroll_item_sub_values'] as $deductionSubValue)
                         <div class="flex-auto">
-                            <div class="text-condensed text-xs sub-text-color">{{$deductionSubValue['label']}}</div>
+                            <div class="text-xs sub-text-color">{{$deductionSubValue['label']}}</div>
                             <div class="text-xs font-numeric">{{$deductionSubValue['value']}}</div>
                         </div>
                         @endforeach
