993228ef0dea7715a77ce5e21c3cd36d891b3d16
Kim@PC <kimdeguzman20@yahoo.com>
2026-03-26 17:04:24 +0800

    Update Payroll Email Subject to Use Draft Terminology
    - Changed email subject in `PayrollGeneratedMail` to include "draft" for consistency with updated terminology.

--- app/Mail/PayrollGeneratedMail.php
+++ app/Mail/PayrollGeneratedMail.php
@@ -29,7 +29,7 @@ class PayrollGeneratedMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: "Payroll#: {$this->payroll->number} generated.",
+            subject: "Payroll draft#: {$this->payroll->number} generated.",
         );
     }
 
